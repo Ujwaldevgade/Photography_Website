2d7aab31397d1464acf5aaed61cfffa903a42df1
Ujwal Devgade <ujwaldevgade@gmail.com>
2024-07-24 20:37:05 +0530

    Update project with latest changes

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import './Title.css'; // Import the CSS file for styling
-import backgroundImage from '../assets/5.jpg'; // Update the path to your image
+import backgroundImage from '../assets/background2.jpg'; // Update the path to your image
 
 const Title = () => (
-  <div className="title-container">
-    <img src={backgroundImage} alt="Background" className="background-image" />
+  <div className="title-container" style={{ backgroundImage: `url(${backgroundImage})` }}>
     <div className="overlay">
       <h1 className="typing-effect">Welcome to Ketan Khasale Photography</h1>
       <p>Capturing the moments that captivate your heart</p>
